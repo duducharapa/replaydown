059dd2379bd0eb4e089abc279e0e705cd8ac72ec
Eduardo Charapa <eduardocharapa@gmail.com>
2019-07-21 18:00:32 -0300

    Alterando porta de escuta do servidor

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ import Format from './src/routes/Format';
 import WinMode from './src/routes/WinMode';
 import Scores from './src/routes/Scores';
 
+import Teste from './src/routes/Teste';
+
 /*  CONSTANTES    */
 const app = Express();
 
@@ -38,6 +40,8 @@ app.get('/winmode/:replay',WinMode);
 /*  ROTA QUE RETORNA SCORES DOS JOGADORES  */
 app.get('/scores/:replay',Scores);
 
+app.get('/teste/:replay',Teste);
+
 app.use( (req,res) => res.status(404).send('Route not found') );
 
-app.listen(3000, () => console.log('App iniciado na porta 3000') );
+app.listen(80, () => console.log('App iniciado na porta 3000') );
